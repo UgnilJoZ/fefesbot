2ded8ce69cfa251ea0544070ee25dd19de5ab27a
Jan Ole Zabel <jan.zabel@posteo.de>
2021-09-22 14:49:29 +0200

    Send formatted messages

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Fefes Bot
 Ein kleiner Matrix-Bot, um den RSS-Feed von Fefes Blog in einen Matrix-Raum zu senden
 ## Dependencies
-python-matrix-nio
+python-matrix-nio, python-beautifulsoup4, python-requests
--- bot.py
+++ bot.py
@@ -1,10 +1,10 @@
 import asyncio
 from nio import AsyncClient
 import feedparser
-
 from typing import List
-
 import json
+from bs4 import BeautifulSoup
+import requests
 
 with open('config.json') as js:
     config = json.load(js)
@@ -15,6 +15,13 @@ def get_latest_post_id() -> str:
         return open("latest_post","r").read().strip()
     except FileNotFoundError:
         return ""
+
+def get_formatted_blogpost(url: str) -> str:
+    "Retrieve the given blog post as HTML"
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text, 'html.parser')
+    blogpost = "".join(str(item) for item in soup.li.contents)
+    return blogpost
 
 def get_new_blogposts() -> List[dict]:
     "Get all posts since the last handled post"
@@ -37,13 +44,15 @@ def get_client() -> AsyncClient:
     client.device_id = config['user']
     return client
 
-async def post_entry(client, msg: str) -> None:
+async def post_entry(client, msg: str, formatted: str) -> None:
     await client.room_send(
         room_id=config['room'],
         message_type="m.room.message",
         content={
             "msgtype": "m.text",
-            "body": msg
+            "body": msg,
+            "format": "org.matrix.custom.html",
+            "formatted_body": formatted
         }
     )
 
@@ -51,7 +60,7 @@ async def main() -> None:
     if posts := get_new_blogposts():
         client = get_client()
         for post in posts:
-            await post_entry(client, post['title'])
+            await post_entry(client, post['title'], get_formatted_blogpost(post['link']))
             print(f"Posted {post['id']}.")
         save_latest_post_id(posts[-1]['id'])
         await client.close()
